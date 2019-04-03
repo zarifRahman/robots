d56828b35dc5e550c7425f16d6eb13d1f3fe3ddd
zarifRahman <zarif.shahriar.rahman@gmail.com>
2019-04-03 10:29:58 +0600

    API - fetch used in componentDidMount

--- src/App.js
+++ src/App.js
@@ -7,12 +7,17 @@ class App extends React.Component {
   constructor(){
     super()
     this.state = {
-      robots: robots,
+      robots: [],
       searchField : ' ',
     }
   }
+  componentDidMount(){
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(users => this.setState({ robots: users }) )  /*getting the users from response and updating the users */  
+  }
+
   onSearchChange = (event) => {
-      
       this.setState({
         searchField : event.target.value
       })
@@ -23,16 +28,20 @@ class App extends React.Component {
       return robot.name.toLocaleLowerCase().includes(this.state.searchField.toLocaleLowerCase())
     })
 
-    return (
-      <div className='tc'>
-        <h1>RoboFriends</h1>
-        <SearchBox 
-          searchChange={this.onSearchChange}
-          inputValue={this.state.searchField}
-        />
-        <CardList robots={filterRobots} />
-      </div>
-    )
+    if(this.state.robots.length === 0){
+      return <h1>Loading...</h1>
+    }else{
+      return (
+        <div className='tc'>
+          <h1>RoboFriends</h1>
+          <SearchBox 
+            searchChange={this.onSearchChange}
+            inputValue={this.state.searchField}
+          />
+          <CardList robots={filterRobots} />
+        </div>
+      )
+    }
   }
     
 }
